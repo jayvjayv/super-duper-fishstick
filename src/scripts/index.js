28b550fcd0d24616ff4c3c3f3d693f28a16f9c5c
jay <jayvvan29@gmail.com>
2024-01-28 01:02:06 -0500

    Add firebase CDN links to index.js, Add DOM elements
    Use firebase CDN for initializing app, getting the database and linking the Realtiem database to the index.js file
    
    Add functionality to push data from the input field to the database
    
    Remove template for list items in the unordered list

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,23 @@
-// javascript
+//Firebase Setup
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://playground-5069d-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsements = ref(database, "endorsements")
+
+//DOM Elements
+const input = document.getElementById("endor-field");
+const publishBtn = document.getElementById("publish-btn");
+const endorsementList = document.getElementById("endor-list");
+
+input.addEventListener("click", function(){
+    push(endorsements, input.value);
+    endorsementList.innerHTML += `<li>${input.value}</li>`
+    input.value = ""
+    
+})
